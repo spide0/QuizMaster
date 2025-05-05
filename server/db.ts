1b6514cc7ecd8ed98816cb31b03452339a72b690
spide0 <rabiulislamarnab456@gmail.com>
2025-05-05 23:44:23 +0600

    first commit

--- server/db.ts
+++ server/db.ts
@@ -2,6 +2,8 @@ import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
+import env from "dotenv";
+env.config();
 
 neonConfig.webSocketConstructor = ws;
 
